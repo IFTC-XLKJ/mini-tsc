0a10148607aab3563d5db4fc57d81956cdf2b720
IFTC-XLKJ <iftcceo@139.com>
2026-09-07 14:56:02 +0800

    update net

--- runtime/src/builtins/node_net.c
+++ runtime/src/builtins/node_net.c
@@ -1,60 +1,196 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "node_net.h"
+#include "ts_features.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <windows.h>
 #pragma comment(lib, "ws2_32.lib")
 typedef int socklen_t;
 #define CLOSE_SOCKET closesocket
+#define NET_SOCK_WOULD WSAEWOULDBLOCK
+#define _strcasecmp _stricmp
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <strings.h>
 #define CLOSE_SOCKET close
+#define NET_SOCK_WOULD EWOULDBLOCK
 #endif
 
-Value node_net_createServer(Value callback) {
-  TSHashMap* server = ts_hashmap_new();
-  ts_hashmap_set(server, ts_string_new("_callback"), callback);
+/* ------------------------------------------------------------------ */
+/* Internal structs (stored behind the TS-visible object's "_impl"   */
+/* hashmap key).                                                      */
+/* ------------------------------------------------------------------ */
+typedef struct NetConn NetConn;
+typedef struct NetServer NetServer;
 
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    TS_THROW(ts_value_string(ts_string_new("Failed to create socket")));
-    return ts_value_undefined();
+typedef struct NetAccept {
+  int fd;
+  struct sockaddr_in addr;
+  struct NetAccept* next;
+} NetAccept;
+
+struct NetServer {
+  int type_tag;   /* NET_SERVER_TAG */
+  int fd;
+  int listening;
+  int closed;
+  Value callback;    /* createServer connection listener */
+  TSHashMap* listeners;
+  int backlog;
+  NetAccept* q_head;
+  NetAccept* q_tail;
+#ifdef _WIN32
+  CRITICAL_SECTION q_mu;
+  HANDLE thread;
+#else
+  pthread_mutex_t q_mu;
+  pthread_t thread;
+#endif
+  NetServer* next;
+};
+
+struct NetConn {
+  int type_tag;   /* NET_CONN_TAG */
+  int fd;
+  int closed;
+  int isClient;
+  int connectFired;
+  TSHashMap* listeners;   /* owned by NetConn, mirrored on the object */
+  NetConn* next;
+};
+
+#define NET_SERVER_TAG 0x4E455446  /* 'NETS' */
+#define NET_CONN_TAG   0x4E434E54  /* 'NCNT' */
+
+static NetServer* g_servers = NULL;
+static NetConn*   g_conns   = NULL;
+static int g_net_wsainited = 0;
+
+/* ------------------------------------------------------------------ */
+/* Platform helpers                                                    */
+/* ------------------------------------------------------------------ */
+static void net_ensure_wsa(void) {
+#ifdef _WIN32
+  if (!g_net_wsainited) {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    g_net_wsainited = 1;
   }
-
-  int opt = 1;
-  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
-
-  ts_hashmap_set(server, ts_string_new("_fd"), ts_value_number((double)server_fd));
-  return ts_value_object(server);
+#endif
 }
 
-Value node_net_createConnection(Value options, Value callback) {
-  TSHashMap* optionsMap = (TSHashMap*)options.as.object;
-  TSString* host = ts_to_string(ts_hashmap_get(optionsMap, ts_string_new("host")));
-  double port = ts_to_number(ts_hashmap_get(optionsMap, ts_string_new("port")));
+static void net_set_nonblocking(int fd) {
+#ifdef _WIN32
+  u_long mode = 1;
+  ioctlsocket((SOCKET)fd, FIONBIO, &mode);
+#else
+  int fl = fcntl(fd, F_GETFL, 0);
+  if (fl >= 0) fcntl(fd, F_SETFL, fl | O_NONBLOCK);
+#endif
+}
 
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (sock < 0) {
-    TS_THROW(ts_value_string(ts_string_new("Failed to create socket")));
-    return ts_value_undefined();
+static const char* net_addr_string(struct sockaddr_in* a, char* out, int cap) {
+  char tmp[INET_ADDRSTRLEN];
+  const char* p = inet_ntop(AF_INET, &a->sin_addr, tmp, sizeof(tmp));
+  snprintf(out, cap, "%s", p ? p : "0.0.0.0");
+  return out;
+}
+
+static unsigned short net_local_port(int fd) {
+  struct sockaddr_in a;
+  socklen_t len = sizeof(a);
+  memset(&a, 0, sizeof(a));
+  if (getsockname((SOCKET)fd, (struct sockaddr*)&a, &len) == 0)
+    return ntohs(a.sin_port);
+  return 0;
+}
+
+static unsigned short net_remote_port(int fd) {
+  struct sockaddr_in a;
+  socklen_t len = sizeof(a);
+  memset(&a, 0, sizeof(a));
+  if (getpeername((SOCKET)fd, (struct sockaddr*)&a, &len) == 0)
+    return ntohs(a.sin_port);
+  return 0;
+}
+
+/* ------------------------------------------------------------------ */
+/* Listener helpers (listeners hashmap: event -> TSArray of callbacks) */
+/* ------------------------------------------------------------------ */
+static void net_add_listener(TSHashMap* listeners, const char* ev, Value cb) {
+  if (!listeners || ev == NULL) return;
+  TSString* key = ts_string_new(ev);
+  Value arrVal = ts_hashmap_get(listeners, key);
+  TSArray* arr = NULL;
+  if (arrVal.tag == TAG_ARRAY && arrVal.as.array) {
+    arr = arrVal.as.array;
+  } else {
+    arr = ts_array_new();
+    ts_hashmap_set(listeners, key, ts_value_array(arr));
   }
+  ts_array_push(arr, cb);
+}
 
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons((uint16_t)port);
-  inet_pton(AF_INET, host->data, &addr.sin_addr);
-
-  if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-    CLOSE_SOCKET(sock);
-    TS_THROW(ts_value_string(ts_string_new("Connection failed")));
-    return ts_value_undefined();
+static void net_remove_listener(TSHashMap* listeners, const char* ev, Value cb) {
+  if (!listeners || ev == NULL) return;
+  TSString* key = ts_string_new(ev);
+  Value arrVal = ts_hashmap_get(listeners, key);
+  if (arrVal.tag == TAG_ARRAY && arrVal.as.array) {
+    TSArray* arr = arrVal.as.array;
+    for (int i = 0; i < arr->length; i++) {
+      Value cur = ts_array_get(arr, i);
+      int match = 0;
+      if (cb.tag == TAG_FUNCTION && cur.tag == TAG_FUNCTION)
+        match = (cur.as.function == cb.as.function);
+      else if (cb.tag == TAG_OBJECT && cur.tag == TAG_OBJECT)
+        match = (cur.as.object == cb.as.object);
+      if (match) {
+        TSArray* neu = ts_array_new();
+        for (int j = 0; j < arr->length; j++)
+          if (j != i) ts_array_push(neu, ts_array_get(arr, j));
+        ts_hashmap_set(listeners, key, ts_value_array(neu));
+        break;
+      }
+    }
   }
+}
 
-  TSHashMap* socket = ts_hashmap_new();
-  ts_hashmap_set(socket, ts_string_new("_fd"), ts_value_number((double)sock));
-  return ts_value_object(socket);
+static void net_fire_listeners(TSHashMap* listeners, const char* ev, Value* args, int argc) {
+  if (!listeners || ev == NULL) return;
+  const size_t key_buf = 256;
+  char onceKey[256];
+  /* Regular listeners */
+  Value arrVal = ts_hashmap_get(listeners, ts_string_new(ev));
+  if (arrVal.tag == TAG_ARRAY && arrVal.as.array) {
+    TSArray* arr = arrVal.as.array;
+    for (int i = 0; i < arr->length; i++) {
+      Value fn = ts_array_get(arr, i);
+      if (fn.tag == TAG_FUNCTION && fn.as.function)
+        ts_value_call(fn, args, argc);
+    }
+  }
+  /* once listeners: fire, then clear */
+  snprintf(onceKey, key_buf, "%s##once", ev);
+  Value onceVal = ts_hashmap_get(listeners, ts_string_new(onceKey));
+  if (onceVal.tag == TAG_ARRAY && onceVal.as.array) {
+    TSArray* oarr = onceVal.as.array;
+    for (int i = 0; i < oarr->length; i++) {
+      Value fn = ts_array_get(oarr, i);
+      if (fn.tag == TAG_FUNCTION && fn.as.function)
+        ts_value_call(fn, args, argc);
+    }
+    ts_hashmap_set(listeners, ts_string_new(onceKey), ts_value_array(ts_array_new()));
+  }
 }
