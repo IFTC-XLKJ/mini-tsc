d3b833342c65eb2f71fe63d72a6cc55a9f241454
IFTC-XLKJ <iftcceo@139.com>
2026-09-07 14:57:09 +0800

    update net

--- runtime/src/builtins/node_net.c
+++ runtime/src/builtins/node_net.c
@@ -338,6 +338,172 @@ static Value net_server_new(Value callback) {
 #ifdef _WIN32
   InitializeCriticalSection(&s->q_mu);
   s->thread = NULL;
+/* ====================================================================== */
+/*  Top-level module functions                                            */
+/* ====================================================================== */
+Value node_net_createServer(Value callback) {
+  net_ensure_wsa();
+  return net_server_new(callback);
+}
+
+/* Parse port argument which may be a number, or a 2-arg listen(port, cb) */
+static int net_resolve_port(Value portVal) {
+  if (portVal.tag == TAG_STRING && portVal.as.string) {
+    const char* s = portVal.as.string->data ? portVal.as.string->data : "";
+    return atoi(s);
+  }
+  return (int)ts_to_number(portVal);
+}
+
+Value node_net_server_listen(Value serverVal, Value portVal, Value callback) {
+  NetServer* s = net_server_from(serverVal);
+  if (!s) {
+    TS_THROW(ts_value_string(ts_string_new("Invalid net Server object")));
+    return ts_value_undefined();
+  }
+  net_ensure_wsa();
+  if (s->closed) {
+    TS_THROW(ts_value_string(ts_string_new("Server already closed")));
+    return ts_value_undefined();
+  }
+  if (s->listening) return serverVal;
+
+  int port = net_resolve_port(portVal);
+  s->fd = (int)socket(AF_INET, SOCK_STREAM, 0);
+  if (s->fd < 0) {
+    TS_THROW(ts_value_string(ts_string_new("Failed to create server socket")));
+    return ts_value_undefined();
+  }
+  int opt = 1;
+  setsockopt(s->fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
+
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons((uint16_t)port);
+
+  if (bind(s->fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    CLOSE_SOCKET(s->fd);
+    s->fd = -1;
+    Value err = ts_value_string(ts_string_new("Failed to bind"));
+    net_fire_listeners(s->listeners, "error", &err, 1);
+    TS_THROW(err);
+    return ts_value_undefined();
+  }
+  if (listen(s->fd, s->backlog) < 0) {
+    CLOSE_SOCKET(s->fd);
+    s->fd = -1;
+    TS_THROW(ts_value_string(ts_string_new("Failed to listen")));
+    return ts_value_undefined();
+  }
+  s->listening = 1;
+  ts_hashmap_set((TSHashMap*)serverVal.as.object, ts_string_new("listening"), ts_value_boolean(1));
+
+  /* store the newly bound local port on the object so address() works */
+  unsigned short lp = net_local_port(s->fd);
+  ts_hashmap_set((TSHashMap*)serverVal.as.object, ts_string_new("localPort"), ts_value_number((double)lp));
+
+  /* Launch accept thread */
+#ifdef _WIN32
+  s->thread = CreateThread(NULL, 0, net_accept_thread, s, 0, NULL);
+#else
+  pthread_create(&s->thread, NULL, net_accept_thread, s);
+#endif
+
+  /* Fire listening event */
+  net_fire_listeners(s->listeners, "listening", NULL, 0);
+  /* Listen callback */
+  if (callback.tag == TAG_FUNCTION && callback.as.function) {
+    ts_value_call(callback, NULL, 0);
+    /* Also treat the first arg as the connection listener if passed */
+    if (s->callback.tag != TAG_FUNCTION && s->callback.as.function == NULL) {
+      s->callback = callback;
+    }
+  }
+  return serverVal;
+}
+
+Value node_net_server_on(Value serverVal, Value event, Value callback) {
+  NetServer* s = net_server_from(serverVal);
+  if (!s) return serverVal;
+  TSString* ev = ts_to_string(event);
+  if (ev && ev->data && callback.tag == TAG_FUNCTION) {
+    if (strcmp(ev->data, "connection") == 0) {
+      /* store as the default connection listener too */
+      s->callback = callback;
+    }
+    net_add_listener(s->listeners, ev->data, callback);
+  }
+  return serverVal;
+}
+
+Value node_net_server_once(Value serverVal, Value event, Value callback) {
+  NetServer* s = net_server_from(serverVal);
+  if (!s) return serverVal;
+  TSString* ev = ts_to_string(event);
+  if (ev && ev->data && callback.tag == TAG_FUNCTION) {
+    net_add_listener(s->listeners, ev->data, callback);
+  }
+  return serverVal;
+}
+
+Value node_net_server_off(Value serverVal, Value event, Value callback) {
+  NetServer* s = net_server_from(serverVal);
+  if (!s) return serverVal;
+  TSString* ev = ts_to_string(event);
+  if (ev && ev->data) {
+    net_remove_listener(s->listeners, ev->data, callback);
+  }
+  return serverVal;
+}
+
+Value node_net_server_close(Value serverVal, Value callback) {
+  NetServer* s = net_server_from(serverVal);
+  if (!s) return serverVal;
+  if (!s->closed) {
+    s->closed = 1;
+    if (s->fd >= 0) { CLOSE_SOCKET(s->fd); s->fd = -1; }
+    s->listening = 0;
+    ts_hashmap_set((TSHashMap*)serverVal.as.object, ts_string_new("listening"), ts_value_boolean(0));
+    net_fire_listeners(s->listeners, "close", NULL, 0);
+  }
+  if (callback.tag == TAG_FUNCTION && callback.as.function) {
+    ts_value_call(callback, NULL, 0);
+  }
+  return serverVal;
+}
+
+Value node_net_server_address(Value serverVal) {
+  NetServer* s = net_server_from(serverVal);
+  if (!s) return ts_value_null();
+  TSHashMap* info = ts_hashmap_new();
+  char host[64];
+  unsigned short p = s->listening ? net_local_port(s->fd) : 0;
+  snprintf(host, sizeof(host), "0.0.0.0");
+  ts_hashmap_set(info, ts_string_new("address"), ts_value_string(ts_string_new(host)));
+  ts_hashmap_set(info, ts_string_new("port"), ts_value_number((double)p));
+  ts_hashmap_set(info, ts_string_new("family"), ts_value_string(ts_string_new(s->listening ? "IPv4" : "")));
+  return ts_value_object(info);
+}
+
+Value node_net_server_getConnections(Value serverVal, Value callback) {
+  NetServer* s = net_server_from(serverVal);
+  int count = 0;
+  for (NetConn* c = g_conns; c; c = c->next) {
+    if (!c->isClient && !c->closed) count++;
+  }
+  if (callback.tag == TAG_FUNCTION && callback.as.function) {
+    Value args[2];
+    args[0] = ts_value_null();
+    args[1] = ts_value_number((double)count);
+    ts_value_call(callback, args, 2);
+  }
+  return ts_value_number((double)count);
+}
+
+Value node_net_server_ref(Value serverVal) { return serverVal; }
+Value node_net_server_unref(Value serverVal) { return serverVal; }
 #else
   pthread_mutex_init(&s->q_mu, NULL);
 #endif
